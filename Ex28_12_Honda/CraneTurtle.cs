fb95621b003edade665d778cfdc51315ecedea50
rinera182@gmail.com <rinera182@gmail.com>
2021-12-13 08:02:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Ex28_12_Honda/CraneTurtle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex28_12_Honda
+{
+    class CraneTurtle
+    {
+        private int legsA;  // 動物Aの脚の数
+        private int legsB;  // 動物Bの脚の数
+        private float _headsA = 0;  // 動物Aの頭数（計算で求められる）
+        public float headsA
+        {
+            get
+            {
+                return _headsA;
+            }
+            private set
+            {
+                _headsA = value;
+            }
+        }
+        private float _headsB = 0;  // 動物Bの頭数（計算で求められる）
+        public float headsB
+        {
+            get
+            {
+                return _headsB;
+            }
+            private set
+            {
+                _headsB = value;
+            }
+        }
+        public int _heads = 0;  // 全部の頭の数
+        public int heads
+        {
+            get 
+            {
+                return _heads;
+            }
+            set
+            {
+                if(_heads != value)
+                {
+                    _heads = value;
+                    CreatureCalculation();
+                }
+            }
+        }
+        public int _legs = 0;  // 全部の脚の数
+        public int legs
+        {
+            get
+            {
+                return _legs;
+            }
+            set
+            {
+                if(_legs != value)
+                {
+                    _legs = value;
+                    CreatureCalculation();
+                }
+            }
+        }
+
+        public CraneTurtle(int a, int b)
+        {
+            legsA = a;
+            legsB = b;
+        }
+        public CraneTurtle(int a, int b, int h, int l)
+        {
+            legsA = a;
+            legsB = b;
+            heads = h;
+            legs = l;
+        }
+        public void SetHeadsLegs(int h, int l)
+        {
+            _heads = h;
+            _legs = l;
+            CreatureCalculation();
+        }
+
+        public void CreatureCalculation()
+        {
+            _headsB = (_legs - _heads * legsA) / (legsB - legsA);
+            _headsA = _heads - _headsB;
+        }
+    }
+}
---
+++ Ex28_12_Honda/Ex28.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace Ex28_12_Honda
+{
+    class Ex28
+    {
+        static void Main(string[] args)
+        {
+            CraneTurtle craneTurtle = new CraneTurtle(2, 4);
+            craneTurtle.SetHeadsLegs(5, 16);
+            Console.WriteLine($"鶴の数は{craneTurtle.headsA}羽"); //2で正解
+            Console.WriteLine($"亀の数は{craneTurtle.headsB}匹"); //3で正解
+            craneTurtle.heads = 7;  //頭や足の数を変えても答えが出る
+            Console.WriteLine($"鶴の数は{craneTurtle.headsA}羽"); //6で正解
+            Console.WriteLine($"亀の数は{craneTurtle.headsB}匹"); //1で正解
+            craneTurtle.legs = 20;
+            Console.WriteLine($"鶴の数は{craneTurtle.headsA}羽"); //4で正解
+            Console.WriteLine($"亀の数は{craneTurtle.headsB}匹"); //3で正解
+            CraneTurtle craneTurtle1 = new CraneTurtle(8, 10);
+            craneTurtle1.SetHeadsLegs(7, 66);
+            Console.WriteLine($"タコの数は{craneTurtle1.headsA}匹");
+            Console.WriteLine($"イカの数は{craneTurtle1.headsB}匹");
+            craneTurtle1.heads = 10;
+            craneTurtle1.legs = 86;
+            Console.WriteLine($"タコの数は{craneTurtle1.headsA}匹");
+            Console.WriteLine($"イカの数は{craneTurtle1.headsB}匹");
+        }
+    }
+}
